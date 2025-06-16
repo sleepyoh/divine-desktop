992df471c99447115ebb936376910af8d45e11f3
sleepy <lukas.svardkvist@gmail.com>
2025-06-16 20:13:57 +0200

    random

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,17 +5,15 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "Starting build process..."
 
-echo "Installing packages..."
-bash "$SCRIPT_DIR/packages.sh"
 
-echo "replaceing flatpaks..."
+echo "replacing flatpaks..."
 bash "$SCRIPT_DIR/flatpakreplace.sh"
 
-echo "place holders..."
-bash "$SCRIPT_DIR/placeholder.sh"
+echo "installing mesa+codecs..."
+bash "$SCRIPT_DIR/enarpmfus.sh"
 
-echo "placeholder..."
-bash "$SCRIPT_DIR/placeholder.sh"
+echo "Installing packages..."
+bash "$SCRIPT_DIR/packages.sh"
 
 echo "Cleaning up..."
 bash "$SCRIPT_DIR/cleanup.sh"
