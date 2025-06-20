07a02016e7b5833b87450038a5900b67fd09fd2c
sleepy <lukas.svardkvist@gmail.com>
2025-06-20 16:57:38 +0200

    track fedora 42 explicitly

--- build_files/kernel.sh
+++ build_files/kernel.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+#!/usr/bin/bash
 set -ouex pipefail
 
 echo "🔑 Enabling Nobara COPR for kernel..."
 dnf -y copr enable gloriouseggroll/nobara-42
 
-
 echo "🌀 Installing Nobara kernel and replacing stock kernel if needed..."
-dnf install -y kernel --enablerepo=copr:copr.fedorainfracloud.org:gloriouseggroll:nobara-42 --allowerasing
-
-
+# Use `dnf swap` instead of plain install to properly replace the kernel
+dnf -y swap kernel kernel --enablerepo=copr:copr.fedorainfracloud.org:gloriouseggroll:nobara-42 --allowerasing
