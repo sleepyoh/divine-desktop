26757a4809c41a38cd8376e100ad71bab4394643
sleepy <lukas.svardkvist@gmail.com>
2025-06-20 17:11:09 +0200

    trying kernel again

--- build_files/kernel.sh
+++ build_files/kernel.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 set -ouex pipefail
 
-echo "🔑 Enabling Nobara COPR for kernel..."
+echo "🔑 Enabling Nobara COPR..."
 dnf -y copr enable gloriouseggroll/nobara-42
 
-echo "🌀 Installing Nobara kernel and replacing stock kernel if needed..."
-# Use `dnf swap` instead of plain install to properly replace the kernel
-dnf -y swap kernel kernel --enablerepo=copr:copr.fedorainfracloud.org:gloriouseggroll:nobara-42 --allowerasing
+echo "🔍 Checking available kernels..."
+dnf --enablerepo=copr:copr.fedorainfracloud.org:gloriouseggroll:nobara-42 list kernel
+
+echo "🌀 Installing Nobara kernel, replacing stock Fedora kernel if needed..."
+dnf install -y kernel --best --refresh --enablerepo=copr:copr.fedorainfracloud.org:gloriouseggroll:nobara-42 --allowerasing
