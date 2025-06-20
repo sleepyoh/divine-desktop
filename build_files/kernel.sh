d71814d859f8b94a90e8d461de11f262dd46c1c3
sleepy <lukas.svardkvist@gmail.com>
2025-06-20 16:49:09 +0200

    track fedora 42 explicitly

--- build_files/kernel.sh
+++ build_files/kernel.sh
@@ -8,5 +8,4 @@ dnf -y copr enable gloriouseggroll/nobara-42
 echo "🌀 Installing Nobara kernel and replacing stock kernel if needed..."
 dnf install -y kernel --enablerepo=copr:copr.fedorainfracloud.org:gloriouseggroll:nobara-42 --allowerasing
 
-echo "✅ Done! Kernel info:"
-rpm -q kernel
+
