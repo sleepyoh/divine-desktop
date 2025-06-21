e5c844b38e1b5b8c1cbd37766836fbab3ead22ff
sleepyoh <lukas.svardkvist@gmail.com>
2025-06-21 15:19:36 +0200

    stock kernel

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 #Removing repos after build
 
 #dnf5 -y copr remove ublue-os/packages
-dnf5 -y copr remove bieszczaders/kernel-cachyos
+#dnf5 -y copr remove bieszczaders/kernel-cachyos
 
 ### Cleaning
 # Clean package manager cache
