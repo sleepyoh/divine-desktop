4e02d02d94bdfe129f766c01ecd7301b900d147c
sleepyoh <lukas.svardkvist@gmail.com>
2025-06-21 19:30:54 +0200

    Update copr.sh

--- build_files/copr.sh
+++ build_files/copr.sh
@@ -9,19 +9,14 @@ set -ouex pipefail
 
     #Trying to see if you can use CachyOS kernel, for fun.
 
+    setsebool -P domain_kernel_load_modules on
 
-setsebool -P domain_kernel_load_modules on
+    dnf5 -y copr enable bieszczaders/kernel-cachyos
 
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-
-rpm-ostree override remove \
- kernel \
- kernel-core \
- kernel-modules \
- kernel-modules-core \
- kernel-modules-extra \
- --install kernel-cachyos \
- --install kernel-cachyos-core \
- --install kernel-cachyos-modules \
- --install kernel-cachyos-modules-core \
- --install kernel-cachyos-modules-extra \
+    rpm-ostree override remove \
+     kernel \
+     kernel-core \
+     kernel-modules \
+     kernel-modules-core \
+     kernel-modules-extra \
+     --install kernel-cachyos
