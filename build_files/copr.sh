f92cebf335de232436132ca77aac48d6f650b290
sleepyoh <lukas.svardkvist@gmail.com>
2025-06-21 15:13:05 +0200

    Update copr.sh

--- build_files/copr.sh
+++ build_files/copr.sh
@@ -20,11 +20,3 @@ set -ouex pipefail
      kernel-modules-core \
      kernel-modules-extra \
      --install kernel-cachyos
-
-# Update initramfs
-KERNEL_SUFFIX=""
-
-QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel${KERNEL_SUFFIX:+-${KERNEL_SUFFIX}}")"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
-chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
