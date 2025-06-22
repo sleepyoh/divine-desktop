b9a848ce3c1b2f6c306a5608d4c8ec5f7b9b0496
sleepyoh <lukas.svardkvist@gmail.com>
2025-06-23 00:50:13 +0200

    rm etc after build instead of in containerfile

--- build_files/06rmetc.sh
+++ build_files/06rmetc.sh
@@ -2,9 +2,12 @@
 set -euox pipefail
 
 rm \
+    /etc/os-release \
     /etc/fedora-release \
     /etc/redhat-release \
     /etc/system-release \
     /etc/system-release-cpe \
     /etc/yum.repos.d/rpmfusion* \
     /etc/yum.repos.d/google.chrome
+
+    cp /usr/share/local/os-release /etc/os-release
