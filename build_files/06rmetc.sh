6d1960cce357cc3208ed19db168f45a57660d4b5
sleepyoh <lukas.svardkvist@gmail.com>
2025-06-23 01:15:09 +0200

    s

--- build_files/06rmetc.sh
+++ build_files/06rmetc.sh
@@ -7,7 +7,12 @@ rm \
     /etc/redhat-release \
     /etc/system-release \
     /etc/system-release-cpe \
+    /usr/lib/fedora-release \
+    /usr/lib/system-release-cpe \
+    /usr/lib/os-release \
     /etc/yum.repos.d/rpmfusion* \
-    /etc/yum.repos.d/fedora-cisco-openh264.repo
+    /etc/yum.repos.d/fedora-cisco-openh264.repo \
 
-    cp /usr/local/share/os-release /etc/os-release
+    cp \ 
+     /usr/local/share/os-release /etc/os-release \
+     /usr/local/share/os-release /usr/lib/os-release
