ff389eddcc5dc70287f820f0e2f5d55ed7b4e479
sleepyoh <36079594+sleepyoh@users.noreply.github.com>
2025-11-12 20:06:21 +0100

    revert cachy script

--- build_files/02cachy-kernel.sh
+++ build_files/02cachy-kernel.sh
@@ -14,22 +14,6 @@ set -ouex pipefail
      kernel-modules-extra \
      --install kernel-cachyos
 
-# Find the latest installed CachyOS kernel version
-NEW_KERNEL_VERSION=$(ls /usr/lib/modules | grep 'cachyos' | tail -n 1)
-
-if [[ -z "${NEW_KERNEL_VERSION}" ]]; then
-    echo "Error: Could not find the CachyOS kernel version in /usr/lib/modules."
-    exit 1
-fi
-
-echo "Found new CachyOS kernel version: ${NEW_KERNEL_VERSION}"
-
-# Run depmod manually to generate module dependencies for the new kernel.
-# -a: Scan all module directories. 
-# -b /: Use the current root directory (/) as the base directory (important for container build).
-depmod -a -b / "${NEW_KERNEL_VERSION}"
-
-
      #Removing repos after build
 
 dnf5 -y copr remove bieszczaders/kernel-cachyos
